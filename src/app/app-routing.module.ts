055587244bb2f4cc0f947fd81ffb6a7812d58c9f
Rafael <rtorrealba@position.cl>
2019-03-29 11:08:19 -0300

    App's routes configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
+//ESTAS SON LAS RUTAS IMPORTADAS DESDE OTRO ARCHIVO
 import { routes } from './app.routes';
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
